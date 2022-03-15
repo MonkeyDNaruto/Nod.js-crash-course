385b161a69a4051ac168d6709f95490459beab76
Nischal Shrestha <nischalshrestha324@gmail.com>
2022-03-15 21:01:10 +0545

    server.js added

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log("Request have been made");
+
+    //set header
+res.setHeader('Content-Type', 'text/html');
+
+let path = './text/';
+switch(req.url) {
+    case '/' :
+      path += 'index.html'
+      break;
+    case '/about' : 
+      path += 'about.html'
+      break;
+    default :
+      path += '404.html'
+      break;
+}
+
+fs.readFile(path, (err, data) => {
+    if(err){
+        console.log(err);
+        res.end();
+    } else {
+        res.write(data);
+        res.end();
+    }
+})
+
+});
+
+
+server.listen(3000, 'localhost', () => {
+    console.log("It is listining");
+});
