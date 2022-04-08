b392b748cf89c073e775eb9b0756b2cb816a49e8
Nischal Shrestha <nischalshrestha324@gmail.com>
2022-04-08 13:01:31 +0545

    blogRoutes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const req = require('express/lib/request');
 const { get, result } = require('lodash');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog.js');
+const blogRoutes = require('./routes/blogRoutes.js')
 const res = require('express/lib/response');
 const { param } = require('express/lib/request');
 const { render } = require('express/lib/response');
@@ -49,28 +49,13 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Home', blogs })
 });
 
-app.get('/blogs', async (req, res) => {
-    const blogs = await Blog.find({}).lean();
 
-
-
-
-    res.render('index', { title: 'Blogs', blogs })
-});
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
 });
 
-app.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
+
 
 app.post('/', (req, res) => {
     console.log(req.body);
@@ -85,44 +70,7 @@ app.post('/', (req, res) => {
         })
 })
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create new blog' })
-});
-app.post('/blogs', (req, res) => {
-
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-      .then(result => {
-          res.render('details', { blog: result, title: 'Blog Details' })
-      })
-      .catch(err => {
-          console.log(err);
-      })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-      .then(result => {
-          res.json({ redirect: '/blogs' })
-      })
-      .catch(err => {
-          console.log(err);
-      })
-})
-
+app.use( "/blogs", blogRoutes);
 
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' })
